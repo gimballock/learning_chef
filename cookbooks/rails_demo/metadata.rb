cbfeabebb9f12029ac08c9f6630d721b70b71a36
Eric Price <eric@platformd.com>
2013-11-05 09:38:33 -0800

    rails-app cookbook

---
+++ cookbooks/rails_demo/metadata.rb
@@ -0,0 +1,9 @@
+name             'rails_demo'
+maintainer       'PlatformD'
+maintainer_email 'eric@platformd.com'
+license          'All rights reserved'
+description      'Installs/Configures rails_demo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends	         'application'
+depends          'application_ruby'
